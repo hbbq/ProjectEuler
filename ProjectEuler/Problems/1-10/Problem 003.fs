06f585575409c7d785dc49b54f225a283c16f83a
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2016-09-14 10:49:56 +0200

    Added execution time display to console

--- ProjectEuler/Problems/1-10/Problem 003.fs
+++ ProjectEuler/Problems/1-10/Problem 003.fs
@@ -1,7 +1,9 @@
 ﻿module Problem_003
 
 let problem =    
-    let x = 600851475143.0
-    {2.0..x} |> Seq.tryFind (fun e -> x % e = 0.0 && Common.isPrime (x/e |> int)) |> (fun e -> x / e.Value)
+    let x = 600851475143L
+    {2L..x} 
+    |> Seq.tryFind (fun e -> x % e = 0L && Common.isPrime (x/e |> int)) 
+    |> (fun e -> x/e.Value)
 
 // 6857
--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -4,15 +4,27 @@ open System
 
 [<EntryPoint>]
 let main argv = 
+    
+    Console.WriteLine "Calulating..."
+
+    let stopwatch = Diagnostics.Stopwatch.StartNew()
 
     //Get result from Problem X
     let result = Problem_001.problem.ToString()
 
+    stopwatch.Stop()
+
+    Console.WriteLine "Done\n"
+
+    let resultText = sprintf "Result: %A\n\nExecution time: %d ms" result stopwatch.ElapsedMilliseconds
+    
     //Write to trace and Console
     Diagnostics.Trace.WriteLine result
-    printfn "%A" result
+    Console.WriteLine resultText
+
 
     //Wait for key in console
+    Console.WriteLine "\nPress any key"
     Console.ReadKey() |> ignore
 
     0
