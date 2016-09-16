c77e0c718a952971b6bdc85abc6946d112de7135
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2016-09-16 14:06:40 +0200

    Problem 12

---
+++ ProjectEuler/Problems/000-100/011-020/Problem 012.fs
@@ -0,0 +1,23 @@
+﻿module Problem_012
+
+open Xunit
+
+let problem =
+    let factorCount (n:int64) = 
+        seq {
+            let fn = float n
+            yield 2
+            yield! seq {
+                for t in ([2.0..sqrt fn] |> List.filter (fun e -> fn % e = 0.0)) do
+                    yield 2
+            }
+        }
+        |> Seq.sum
+    Common.infinite 1 
+    |> Seq.map int64 
+    |> Seq.scan (fun a e -> a + e) 0L
+    |> Seq.tryFind (fun e -> factorCount e > 500)
+    |> fun e -> e.Value
+    
+[<Fact>]
+let Test() = Assert.Equal(problem, 76576500L)
