7201259427fc4e328b9d9eb74348609e480c463e
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2016-09-16 16:40:56 +0200

    stuff

--- ProjectEuler/Problems/000-100/061-070/Problem 069.fs
+++ ProjectEuler/Problems/000-100/061-070/Problem 069.fs
@@ -3,15 +3,21 @@
 open Xunit
 
 let problem =
-    let seq = Common.primeSequence |> Seq.takeWhile (fun e -> e <= 500000L) |> Seq.map int |> Seq.toList
-    let findFactors n = seq |> List.filter (fun e -> n % e = 0)
-    let facts = [2..1000000] |> List.map (fun e -> e, findFactors e) |> Map.ofList
-    //let testFactors (x:list<int>) n = x |> List.tryFind (fun e -> n % e = 0) |> fun e -> e.IsNone
-    //let testNum n =
-    //    let f = findFactors n
-    //    [1..n-1] |> List.filter (testFactors f) |> List.length |> fun e -> (float n) / (float e)
-    //[2..100000] |> List.map (fun e -> e, (testNum e)) |> List.sortByDescending (fun (a, b) -> b) |> fun e -> fst e.Head
-    facts
+    let max = 1000000
+    let rec gcd a b =
+        if a = 0 then b
+        else gcd (b%a) a
+    let phi n =
+        [2..n-1]
+        |> List.filter (fun e -> (gcd e n) = 1)
+        |> List.length
+        |> fun e -> e + 1
+    let test n =
+        float(n) / float(phi n)
+    [1..max]
+    |> List.map (fun e -> e, test e)
+    |> List.sortByDescending snd
+    |> List.head
 
 // NOT COMPLETED
        
