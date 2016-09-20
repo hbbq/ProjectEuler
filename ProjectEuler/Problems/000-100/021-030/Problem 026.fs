bf3cddf91f7bfc1dd7299731f364c86afd2d1e66
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2016-09-20 08:10:36 +0200

    problem 26

---
+++ ProjectEuler/Problems/000-100/021-030/Problem 026.fs
@@ -0,0 +1,26 @@
+﻿module Problem_026
+
+open Xunit
+
+let problem =
+    let test (n:int) =
+        let str = (10I**1500) / bigint(n) |> fun e -> e.ToString()
+        if str.EndsWith("00000000000000000") then 1
+        else
+            let nums = 
+                str
+                |> fun e -> e.ToCharArray()
+                |> Array.map (fun e -> int(e.ToString()))
+                |> Array.toList
+                |> List.skip 200
+            let testpattern l =
+                let pat =
+                    let c = nums |> List.take l
+                    c@c@c
+                let ll = min (List.length pat) (List.length nums)
+                (pat |> List.take ll) = (nums |> List.take ll)
+            {1..(List.length nums)} |> Seq.find testpattern
+    [1..999] |> List.map (fun e -> e, test e) |> List.sortByDescending snd |> List.head |> fst
+
+[<Fact>]
+let Test() = Assert.Equal(problem, 983)
