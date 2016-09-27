27adf05b49e8b5b44342109d64d57da655917466
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2016-09-27 07:12:40 +0200

    Problem 31 & 32

---
+++ ProjectEuler/Problems/000-100/031-040/Problem 031.fs
@@ -0,0 +1,17 @@
+﻿module Problem_031
+
+open Xunit
+
+let problem =
+    let coins = [1;2;5;10;20;50;100;200]
+    let find x =
+        let arr : int array = Array.zeroCreate (x + 1)
+        arr.[0] <- 1;
+        for coin in coins do
+            for pos in [coin..x] do
+                arr.[pos] <- (arr.[pos] + arr.[pos-coin])
+        arr.[x]        
+    find 200
+
+[<Fact>]
+let Test() = Assert.Equal(problem, 73682)
---
+++ ProjectEuler/Problems/000-100/031-040/Problem 032.fs
@@ -0,0 +1,24 @@
+﻿module Problem_032
+
+open Xunit
+
+let problem =
+    let getDigits (n:int) =
+        n.ToString().ToCharArray() |> Array.toList
+    let test n =
+        let digs = getDigits n
+        if digs.Length <> (digs |> List.distinct |> List.length) || (digs |> List.tryFind (fun e -> e = '0') |> fun e -> e.IsSome) then
+            false
+        else
+            let factors = Common.getDivisors n |> Seq.takeWhile (fun e -> e <= Common.Int.sqrt n)
+            let test n f =
+                let f2 = n / f
+                let df = getDigits f
+                let df2 = getDigits f2
+                let dt = digs@df@df2
+                dt.Length = 9 && (dt |> List.tryFind (fun e -> e = '0') |> fun e -> e.IsNone) && dt.Length = (dt |> List.distinct |> List.length)
+            factors |> Seq.tryFind (fun e -> test n e) |> fun e -> e.IsSome            
+    [1234..9876] |> List.filter test |> List.distinct |> List.sum
+
+[<Fact>]
+let Test() = Assert.Equal(problem, 45228)
