bb20b7143cb2574cf06b3614d39e7cc1f8487a1c
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2016-10-01 13:16:50 +0200

    Start to rearrange tests

--- ProjectEuler/Problems/000-100/001-010/Problem 001.fs
+++ ProjectEuler/Problems/000-100/001-010/Problem 001.fs
@@ -4,6 +4,3 @@ open Xunit
 
 let problem =
     [0..999] |> List.filter (fun x -> x % 3 = 0 || x % 5 = 0) |> List.sum
-
-[<Fact>]
-let Test() = Assert.Equal(problem, 233168)
---
+++ ProjectEuler/Tests/ProblemsTests.fs
@@ -0,0 +1,6 @@
+﻿module ProblemsTests
+
+open Xunit
+open Shouldly
+
+[<Fact>] let Problem_001() = Problem_001.problem.ShouldBe(233168)
